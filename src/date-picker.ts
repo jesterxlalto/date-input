5aeb9a0afee8f4450b10c0ef0b03be165915251f
Jesse Warden <jesse.warden@ridealto.com>
2022-12-16 10:51:18 -0500

    date-fns parses

--- src/date-picker.ts
+++ src/date-picker.ts
@@ -1,6 +1,6 @@
 import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
-import { format } from 'date-fns'
+import {customElement, property, state} from 'lit/decorators.js';
+import { format, parse } from 'date-fns'
 
 /**
  * An example element.
@@ -26,15 +26,28 @@ export class DatePicker extends LitElement {
   @property({type: String})
   selectedDate = new Date().toISOString()
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({type: Number})
-  count = 0;
+  @state()
+    private _userText = "";
+
+    @state()
+    private _isParseable = true
 
   override render() {
-    return html`<p>${format(new Date(), "M/d/y")}</p>
+    console.log(format(new Date(), "M/d/y"))
+
+    return html`<input @input=${this.parseUserTypedDate}>
+    <p>Is Parseable: ${this._isParseable}</p>
     `;
+  }
+
+  parseUserTypedDate(e) {
+    const wat = parse(e.target.value, 'M/d/y', new Date())
+    console.log("wat:", wat.toString())
+    if(wat.toString() === 'Invalid Date') {
+        this._isParseable = false
+    } else {
+        this._isParseable = true
+    }
   }
 //   <h1>${this.sayHello(this.name)}!</h1>
 //   <button @click=${this._onClick} part="button">
